fa862b9e2a167e0cc944c271ffa9f0c10f15c3b5
Florian Courgey <florian@floriancourgey.com>
2018-01-14 02:12:19 -0500

    + override Hook to display text on Hook::exec (if not ajax)

--- override/classes/Hook.php
+++ override/classes/Hook.php
@@ -14,7 +14,9 @@ class Hook extends HookCore {
     $controller_obj = Context::getContext()->controller;
     // dump($controller_obj);die();
     if($controller_obj && !$controller_obj->ajax){
-      echo "Hook::exec($hook_name, ,$id_module)<br/>\n";
+      echo '<span style="background:red;color:white;padding:1px;">';
+      echo "Hook::exec(\$hook_name=$hook_name, , \$id_module=$id_module)<br/>\n";
+      echo '</span>';
     }
     return parent::exec($hook_name,
     $hook_args,

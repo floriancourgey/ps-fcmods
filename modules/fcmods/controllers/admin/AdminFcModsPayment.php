da68a627d5a493e64dc4b1456a7facca2d16a1f0
Florian Courgey (Fresh) <florian@floriancourgey.com>
2018-01-25 15:58:04 -0500

    payment : + list display customer + titre devis + edit currency et amount

--- modules/fcmods/controllers/admin/AdminFcModsPayment.php
+++ modules/fcmods/controllers/admin/AdminFcModsPayment.php
@@ -10,10 +10,11 @@ class AdminFcModsPaymentController extends ModuleAdminController{
       $this->table = 'order_payment';
       $this->className = 'OrderPayment';
       $this->allow_export = true;
-      // $this->_select = 'concat(c.firstname, " ", c.lastname) as customer ';
-      // $this->_join = '
-		  //   JOIN `'._DB_PREFIX_.'customer` c ON (c.`id_customer` = a.`id_customer`)
-      // ';
+      $this->_select = 'o.titre, concat(c.firstname, " ", c.lastname) as customer';
+      $this->_join = '
+        LEFT JOIN `'._DB_PREFIX_.'orders` o ON (o.`reference` = a.`order_reference`)
+        LEFT JOIN `'._DB_PREFIX_.'customer` c ON (c.`id_customer` = o.`id_customer`)
+      ';
 
       parent::__construct();
 
@@ -34,12 +35,13 @@ class AdminFcModsPaymentController extends ModuleAdminController{
           'payment_method' => ['title' => $this->trans('Payment method', [], 'Admin.Global'),'class' => 'fixed-width-xs'],
           'date_add' => ['title' => $this->trans('Date', [], 'Admin.Global'), 'type'=>'datetime'],
           // 'customer' => ['title' => $this->trans('Customer', [], 'Admin.Global')],
-          'amount' => ['title' => $this->trans('Total', [], 'Admin.Global'),
+          'amount' => ['title' => $this->trans('Amount', [], 'Admin.Global'),
             'align' => 'text-right',
             'type' => 'price',
           'badge_success' => true],
-
+          'customer' => ['title' => $this->trans('Customer', [], 'Admin.Global')],
           'order_reference' => ['title' => $this->trans('Order', [], 'Admin.Global')],
+          'titre' => ['title' => $this->trans('Title', [], 'Admin.Global')],
       ];
 
         $this->addRowAction('edit');
@@ -81,10 +83,17 @@ class AdminFcModsPaymentController extends ModuleAdminController{
 
       $payment_methods = array(); // taken from https://github.com/PrestaShop/PrestaShop/blob/develop/controllers/admin/AdminOrdersController.php#L1742
       foreach (PaymentModule::getInstalledPaymentModules() as $payment) {
-          $module = Module::getInstanceByName($payment['name']);
-          if (Validate::isLoadedObject($module) && $module->active) {
-              $payment_methods[] = ['key'=>$module->displayName, 'name'=>$module->displayName];
-          }
+        $module = Module::getInstanceByName($payment['name']);
+        if (Validate::isLoadedObject($module) && $module->active) {
+          $payment_methods[] = ['key'=>$module->displayName, 'name'=>$module->displayName];
+        }
+      }
+
+      $currencies = [];
+      foreach(Currency::getCurrenciesByIdShop(Context::getContext()->shop->id) as $c){
+        // dump($c)
+        $currencies[] = ['key'=>$c['id_currency'], 'name'=>"{$c['name']} ({$c['iso_code']})"];
+        // dump($c);die();
       }
 
       $this->fields_form = [
@@ -114,6 +123,24 @@ class AdminFcModsPaymentController extends ModuleAdminController{
                     'name' => 'name',
                   ],
                   'label' => $this->trans('Method', [], 'Admin.Global').' '.$this->trans('Payment', [], 'Admin.Global'),
+                  'required' => true,
+              ],
+              [
+                  'name' => 'amount',
+                  'suffix' => $currency->sign,
+                  'type' => 'text',
+                  'label' => $this->trans('Amount', [], 'Admin.Global'),
+                  'required' => true,
+              ],
+              [
+                  'name' => 'id_currency',
+                  'type' => 'select',
+                  'options' => [
+                    'query'=> $currencies,
+                    'id' => 'key',
+                    'name' => 'name',
+                  ],
+                  'label' => $this->trans('Currency', [], 'Admin.Global'),
                   'required' => true,
               ],
           ],
